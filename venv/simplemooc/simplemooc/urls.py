5e82fd01b4c42ecc64b518548d2619f9297ba189
Bruno Silva <brsilva.eng@gmail.com>
2018-12-17 23:38:55 -0200

    organiza urls de acordo com o app

--- venv/simplemooc/simplemooc/urls.py
+++ venv/simplemooc/simplemooc/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from simplemooc.core.views import home, contact
+from django.urls import include, path
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contato/', contact, name='contact'),
+    path('', include('simplemooc.core.urls')),
     path('admin/', admin.site.urls),
 ]
 
